417d8513c23e22e3afdb820dd4fc278313083e73
dee-goyal <142106293+dee-goyal@users.noreply.github.com>
2024-12-22 17:44:40 +0530

    couponmodel.js

--- models/couponmodel.js
+++ models/couponmodel.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
 const couponSchema = new mongoose.Schema({
-  code: { type: String, required: true, unique: true },
-  discountPercentage: { type: Number, required: true },
-  expiryDate: { type: Date, required: true }
+  code: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  discountPercentage: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-// Validate Coupon
-couponSchema.methods.validateCoupon = function (orderAmount) {
-  if (new Date() > this.expiryDate) return { valid: false, message: 'Coupon expired' };
-  return {
-    valid: true,
-    discount: (orderAmount * this.discountPercentage) / 100
-  };
-};
-
 const Coupon = mongoose.model('Coupon', couponSchema);
-
 module.exports = Coupon;
