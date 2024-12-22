f4ebf3ee28f3f57850ff571f075f3e48f6237489
dee-goyal <142106293+dee-goyal@users.noreply.github.com>
2024-12-22 17:39:40 +0530

    product.js

--- models/product.js
+++ models/product.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+
 const productSchema = new mongoose.Schema({
   name: String,
   price: String,
-  img: String,
+  images: [String], // Allow multiple image URLs
   category: String,
   rating: Number,
-  productId: { type: String, unique: true }, // Added productId field
-  inStockValue: Number, // Available stock value
-  soldStockValue: Number, // Number of items sold
-  visibility: { type: String, default: 'on' } // Visibility field with default 'on'
+  productId: { type: String, unique: true },
+  inStockValue: Number,
+  soldStockValue: Number,
+  visibility: { type: String, default: 'on' }
 });
 
 const Product = mongoose.model('Product', productSchema);
+
 module.exports = Product;
